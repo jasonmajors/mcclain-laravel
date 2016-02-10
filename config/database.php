1359ff102476ed7c7d2361ed414d847b1a38839d
Jason Majors <jasonrmajors@gmail.com>
2016-02-09 22:24:11 -0800

    Changed database settings for heroku

--- config/database.php
+++ config/database.php
@@ -54,10 +54,10 @@ return [
 
         'mysql' => [
             'driver'    => 'mysql',
-            'host'      => env('DB_HOST', 'localhost'),
-            'database'  => env('DB_DATABASE', 'forge'),
-            'username'  => env('DB_USERNAME', 'forge'),
-            'password'  => env('DB_PASSWORD', ''),
+            'host'      => parse_url(getenv('DATABASE_URL'))['host'],
+            'database'  => substr(parse_url(getenv('DATABASE_URL'))['path'],
+            'username'  => parse_url(getenv('DATABASE_URL'))['user'],
+            'password'  => parse_url(getenv('DATABASE_URL'))['pass'],
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
