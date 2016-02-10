3d584508973776bb6eaf8e79db72ae885bd050c0
Jason Majors <jasonrmajors@gmail.com>
2016-02-09 21:28:54 -0800

    Changed the host in the .env file rather than hard coding it into the host value

--- config/database.php
+++ config/database.php
@@ -54,7 +54,7 @@ return [
 
         'mysql' => [
             'driver'    => 'mysql',
-            'host'      => '192.168.10.10',
+            'host'      => env('DB_HOST', 'localhost'),
             'database'  => env('DB_DATABASE', 'forge'),
             'username'  => env('DB_USERNAME', 'forge'),
             'password'  => env('DB_PASSWORD', ''),
